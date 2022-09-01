5a9c8ce1d2a441a09a46032d1c332f878a735ae9
Fahim Fuad Prottoy <prottoyfuad@gmail.com>
2022-09-01 18:51:45 +0600

    added LC

--- data_structure/LineContainer.cpp
+++ data_structure/LineContainer.cpp
@@ -16,56 +16,56 @@
 #include <iostream>
 
 inline long long floor_div(long long a, long long b) {
- 	return a / b - ((a ^ b) < 0 && a % b != 0); 
+  return a / b - ((a ^ b) < 0 && a % b != 0); 
 }                         
 
 struct Line {
-	mutable long long k, m, p;
-	bool operator < (const Line& o) const {
+  mutable long long k, m, p;
+  bool operator < (const Line& o) const {
     return k < o.k;
   }
-	bool operator < (long long x) const { 
+  bool operator < (long long x) const { 
     return p < x; 
   }
 };
 
-struct LineContainer : std::multiset<Line, std::less<>> {
-  /*
-	  for doubles, 
+/*
+      for doubles, 
       use inf = 1/.0 and div(a, b) = a / b
-  */
-	static const long long inf = 8e18;       
-	bool isect(iterator x, iterator y) {
-		if (y == end()) {
+*/
+struct LineContainer : std::multiset<Line, std::less<>> {
+  static const long long inf = 8e18;       
+  bool isect(iterator x, iterator y) {
+    if (y == end()) {
       x->p = inf;
       return 0;
     }
-		if (x->k == y->k) {
+    if (x->k == y->k) {
       x->p = (x->m > y->m ? inf : -inf);
     }
-		else {
+    else {
       x->p = floor_div(y->m - x->m, x->k - y->k);
     }
-		return x->p >= y->p;
-	}
-	void add(long long k, long long m) {
-		auto z = emplace(k, m, 0);
+    return x->p >= y->p;
+  }
+  void add(long long k, long long m) {
+    auto z = emplace(k, m, 0);
     auto x = z, y = z++;       
-		while (isect(y, z)) {
+    while (isect(y, z)) {
       z = erase(z);
     }
-		if (x != begin() && isect(--x, y)) {
+    if (x != begin() && isect(--x, y)) {
       isect(x, y = erase(y));
     }
-		while ((y = x) != begin() && (--x)->p >= y->p) {
-			isect(x, erase(y));
+    while ((y = x) != begin() && (--x)->p >= y->p) {
+      isect(x, erase(y));
     }
-	}
-	long long query(long long x) {
-		assert(!empty());
-		auto l = *lower_bound(x);
-		return l.k * x + l.m;
-	}
+  }
+  long long query(long long x) {
+    assert(!empty());
+    auto l = *lower_bound(x);
+    return l.k * x + l.m;
+  }
 };
 
 using namespace std;
@@ -73,10 +73,7 @@ using namespace std;
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
-  int tt;
-  cin >> tt;
-  while (tt--) {
-  }
+  
   return 0;
 }
-  
+
