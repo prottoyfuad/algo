b6db49cde36c2018ce9441c652bb59e5bed07486
prottoyfuad <prottoyfuad@gmail.com>
2023-12-09 18:50:27 +0600

    upd segtree and prefix_tree

--- data_structure/Prefix_tree.cpp
+++ data_structure/Prefix_tree.cpp
@@ -4,6 +4,53 @@
 #include <cassert>
 #include <iostream>
 
+template <const int A> struct Prefix_tree {
+  std::vector<std::array<int, A>> to;
+  std::vector<long long> cnt;
+  int unit = -1;
+   
+  Prefix_tree() {
+    add_node();
+  }
+
+  int add_node() {
+    int k = (int) to.size();
+    to.emplace_back();
+    cnt.push_back(0);
+    for (int i = 0; i < A; i++) {
+      to[k][i] = unit;
+    }
+    return k;                    
+  }
+
+  void add(const std::vector<int>& a) {
+    int i = 0;
+    for (int c : a) {
+      if (to[i][c] == unit) {
+        to[i][c] = add_node();
+      }
+      cnt[i] += 1;
+      i = to[i][c];
+    }
+    cnt[i] += 1;
+  }
+
+  long long get(const std::vector<int>& a) const {
+    long long ans = 0;     
+    int i = 0;
+    for (int c : a) {    
+      if (to[i][c] == unit) {
+        break;
+      }
+      i = to[i][c];
+      ans += cnt[i];
+    }
+    return ans;
+  }
+};
+
+// for adding number as the bits.
+// some xor stuff
 struct Prefix_tree {
   int bits;
   std::vector<std::array<int, 2>> childs;  
@@ -63,15 +110,6 @@ using namespace std;
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
-  int l, r;
-  cin >> l >> r;
-  vector<int> a(r - l + 1);
-  Prefix_tree trie(17);
-  for (int i = 0; i <= r - l; i++) {
-    cin >> a[i];
-    trie.insert(a[i]);
-  }
-  int ans = -1;
   
   return 0;
 }
--- data_structure/Segtree.cpp
+++ data_structure/Segtree.cpp
@@ -1,4 +1,41 @@
-  
+/*
+  implementation 1
+  use monoid as T which contains default constructor, implicit constructors and +operator
+*/
+template <typename T> struct Segtree {
+  int n;
+  std::vector<T> tree;
+  Segtree() : n() {}
+  Segtree(int s) : n(1) {
+    while (n < s) n *= 2;
+    tree = std::vector<T> (n * 2 - 1);
+  }
+  void apply(int u, const T& w) {
+    u += n - 1;
+    tree[u] = w;
+    while (u > 0) {
+      --u >>= 1;
+      int v = u << 1;
+      tree[u] = tree[v + 1] + tree[v + 2];
+    }
+  }
+  T get(int l, int r) {
+    T u, v;
+    l += n - 1, r += n - 1;
+    while (l < r) {
+      if (~l & 1) u = u + tree[l++];
+      if (~r & 1) v = tree[--r] + v;
+      l >>= 1, r >>= 1;
+    }
+    return u + v;
+  }
+  T operator[] (int at) { return get(at, at + 1); }
+};
+
+/*
+  implementation 2
+  send default unit value and unite(+) function as template parameter
+*/  
 template <typename T, 
           typename Fun = std::function<T(const T&, const T&)>>
 struct Segtree {
