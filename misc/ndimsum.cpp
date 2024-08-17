c15f57da41510b50f0fe325e86c8b01a6483e50b
Fahim Fuad Prottoy <prottoyfuad@gmail.com>
2024-08-17 07:06:52 +0600

    improved Ndimsum

--- misc/ndimsum.cpp
+++ misc/ndimsum.cpp
@@ -4,71 +4,70 @@
 
 using namespace std;
 
-struct Ndimsum {
+template <typename T = long long> struct Ndimsum {
   int D, S;
-  vector<int> n, v;
-  vector<long long> a;
+  vector<int> n;
+  vector<T> a;
+
+  void init() {
+    for (int i = 0; i < D; i++) {
+      S *= n[i] + 1;
+    }
+    a.assign(S, 0);
+  }
 
   // D_dimensions, each having N_length
-  explicit Ndimsum(int D_, int N_) : D(D_), S(1) {
-    n.assign(D, N_);
-    for (int i = 0; i < D; i++) {
-      S *= n[i] + 1;
-    }
-    v.assign(D, 0);
-    a.assign(S, 0);
-  }
-
+  explicit Ndimsum(int D_, int N_) : D(D_), S(1), n(D, N_) { init(); }
+  
   // Variable length dimentions, n_[i] being the i'th dimention's length
-  explicit Ndimsum(vector<int> n_) : D(n_.size()), S(1), n(n_) {
-    for (int i = 0; i < D; i++) {
-      S *= n[i] + 1;
-    }
-    v.assign(D, 0);
-    a.assign(S, 0);
-  }
+  explicit Ndimsum(vector<int> n_) : D(n_.size()), S(1), n(n_) { init(); }
 
-  int get(const vector<int>& index) {
+  int get(const vector<int>& v) {
     int at = 0;
     for (int i = 0; i < D; i++) {
       at *= n[i];
-      at += index[i];
+      at += v[i];
     }
     return at;
   }
 
-  long long make(int pos = 0, int del = 0) {
+  T make(vector<int>& v, int pos = 0, int del = 0) {
     if (pos >= D) {
       if (del == 0) {
         return 0;
       }
       return del & 1 ? a[get(v)] : -a[get(v)];
     }
-    long long ans = 0;
+    T ans = 0;
     v[pos]--;
-    ans += make(pos + 1, del + 1);
+    ans += make(v, pos + 1, del + 1);
     v[pos]++;
-    ans += make(pos + 1, del);
+    ans += make(v, pos + 1, del);
     return ans;
   }
 
-  void build_from_input(int pos = 0) {
+  void build_from_input(vector<int>& v, int pos) {
     if (pos >= D) {
-      int x;
+      T x;
       cin >> x;
-      a[get(v)] = make() + x;
+      a[get(v)] = make(v) + x;
       return;
     }
     for (v[pos] = 1; v[pos] <= n[pos]; v[pos]++) {
-      build_from_input(pos + 1);
+      build_from_input(v, pos + 1);
     }
   }
 
-  long long query(vector<int>& l, vector<int>& r, int pos = 0, int del = 0) {
+  void build_from_input() {
+    vector<int> v(D);
+    build_from_input(v, 0);
+  }
+
+  T query(vector<int>& l, vector<int>& r, int pos = 0, int del = 0) {
     if (pos >= D) {
       return a[get(r)] * (del & 1 ? -1 : 1);
     }
-    long long ans = 0;
+    T ans = 0;
     int tmp = r[pos];
     r[pos] = l[pos];
     ans += query(l, r, pos + 1, del + 1);
